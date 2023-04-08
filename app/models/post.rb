8bf202d20b299b03a3a29ffcecafb06dcddddccc
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-04-08 12:02:12 -0400

    commit tree 49d392983b1efbd7f3311d14c62ce91b30daee0b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
+
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :title_is_clickbait
+
+    CLICKBAIT_WORDS = ["Won't Believe", "Secret", "Top ", "Guess"]
+
+    def title_is_clickbait
+        if title
+            if CLICKBAIT_WORDS.none? { |word|
+                title.include?(word) }
+                errors.add(:title, "title isn't clickbait-y")
+            end
+        end
+    end
+
 end
